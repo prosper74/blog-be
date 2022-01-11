e62906f5a99305d8a4063a45057b9bbf890d4348
dinakajoy <odinakajoy909@gmail.com>
2022-01-11 15:50:40 +0100

    added oauth

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL", "http://localhost:1337"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', `${process.env.JWTSECRET}`),
+      secret: env("ADMIN_JWT_SECRET", `${process.env.JWTSECRET}`),
     },
   },
 });
