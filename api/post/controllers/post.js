b3920d65998881444fad731d9a6b4d0299d8ae31
Prosper Atu <atu.prosper@gmail.com>
2022-02-17 20:37:24 +0100

    must work

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,25 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.post.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.post.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+};
